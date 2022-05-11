7d9c951a68c5dcea7a6c39cfe4062557b63ccd5e
developerworks <developerworks@163.com>
2022-05-12 06:00:46 +0800

    refactor

--- macro2/src/main.rs
+++ macro2/src/main.rs
@@ -1,6 +1,12 @@
+use hello_macro::HelloMacro;
 use hello_macro_derive::api;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
 
 fn main() {
+    Pancakes::hello_macro();
     println!("Hello, world!");
     // test();
     get();
